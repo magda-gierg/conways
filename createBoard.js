5ff4d0a164d11f5f84ae18e0af98b0c109e7a297
magda-gierg <mjgiergiczny@gmail.com>
2017-07-08 10:07:21 +1200

    createBoard

--- createBoard.js
+++ createBoard.js
@@ -1,5 +1,11 @@
 function createBoard(size) {
+  var board = new Array(size)
+  for (var i=0; i < size; i++) {
+    board[i]= new Array(size)
+    board[i].fill(false)
+  }
+  return board
+
 }
 
 module.exports = createBoard
-
