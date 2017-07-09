e6403d761ed768757cd311d87df952dc8da82162
magda-gierg <mjgiergiczny@gmail.com>
2017-07-09 19:47:43 +1200

    nextBoard passed

--- displayBoard.js
+++ displayBoard.js
@@ -1,5 +1,10 @@
 //Can you render your board in prettier way?
 function displayBoard(board) {
-  console.log(board)
+  process.stdout.write('\033c');
+  for(var i = 0; i < board.length; i++) {
+    var row = board[i]
+    console.log(row.map((cell) => (cell) ? 'x' : '.').join(' '))
+  }
+  console.log()
 }
-module.exports = displayBoard 
+module.exports = displayBoard
--- nextBoard.js
+++ nextBoard.js
@@ -2,7 +2,15 @@ var nextCellState = require('./nextCellState')
 var countAliveNeighbours = require('./countAliveNeighbours')
 
 function nextBoard(currentBoard) {
-  
+  var newBoard = []
+  for (var i=0; i <currentBoard.length; i++) {
+    newBoard.push([])
+    for (var j=0; j <currentBoard.length; j++) {
+      newBoard[i].push(nextCellState(currentBoard[i][j],countAliveNeighbours(i,j,currentBoard)))
+    }
+  }
+  return newBoard
+
 }
 
 module.exports = nextBoard
